8b2fdb65dfece72f6608fbde1510e3e5a804ce85
Sergey Kovalev <dipdeepcode@mail.ru>
2023-05-20 20:09:49 +0300

    Пример использования библиотеки pmml4s_3 для использования заранее подготовленной библиотеки

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,41 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.pmml4s.model.Model;
+
+import java.util.*;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    private final Model model = Model.fromFile(App.class.getClassLoader().getResource("model.pmml").getFile());
+
+    public Double getRegressionValue(Map<String, Double> values) {
+        Object[] valuesMap = Arrays.stream(model.inputNames())
+                .map(values::get)
+                .toArray();
+
+        Object[] result = model.predict(valuesMap);
+        return (Double) result[0];
+    }
+
+    public static void main(String[] args) {
+
+
+
+        App main = new App();
+        Map<String, Double> values = Map.of(
+                "age", 20d,
+                "sex", 1d,
+                "bmi", -100d,
+                "bp", -200d,
+                "s1", 1d,
+                "s2", 2d,
+                "s3", 3d,
+                "s4", 4d,
+                "s5", 5d,
+                "s6", 6d
+        );
+
+        double predicted = main.getRegressionValue(values);
+        System.out.println(predicted);
     }
 }
